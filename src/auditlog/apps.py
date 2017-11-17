b1488f2805275a2ae101f13d4ed329ac01770609
modgahead <modgahead@gmail.com>
2017-11-17 17:29:36 +0200

    Remove jsonfield_compat

--- src/auditlog/apps.py
+++ src/auditlog/apps.py
@@ -6,7 +6,3 @@ from django.apps import AppConfig
 class AuditlogConfig(AppConfig):
     name = 'auditlog'
     verbose_name = "Audit log"
-
-    def ready(self):
-        import jsonfield_compat
-        jsonfield_compat.register_app(self)
